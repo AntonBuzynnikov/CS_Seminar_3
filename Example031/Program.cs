b41f9a757651a39e51ff251730477edaa418689f
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-04 08:50:19 +0700

    Complete example031

---
+++ Example031/Program.cs
@@ -0,0 +1,13 @@
+﻿// 31. Вывести на экран кубы чисел от 1 до N
+int N = Int32.Parse(System.Console.ReadLine());
+
+System.Console.WriteLine("Число\tКуб");
+for (int i = 1; i <= N; i++)
+{
+    System.Console.WriteLine("{0}\t{1}",i,Cub(i));
+}
+
+double Cub(double x)
+{
+    return Math.Pow(x,3);
+}
