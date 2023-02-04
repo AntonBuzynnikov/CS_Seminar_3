53c6afdef84ba251ac71b58379950b106cc7bcf0
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-04 08:55:12 +0700

    Complete example032

---
+++ Example032/Program.cs
@@ -0,0 +1,13 @@
+﻿// 32. Найти сумму чисел от одного до А
+int A = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine(sum(A));
+
+double sum(double x)
+{
+    int sum0 = 0;
+    for(int i=1;i <= x; i++)
+    {
+        sum0 += i;
+    }
+    return sum0;
+}
