23eba0909adf2c3114bae7c85081eb95607c110f
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-04 09:01:06 +0700

    Complete example036

---
+++ Example036/Program.cs
@@ -0,0 +1,14 @@
+﻿// 36. Написать программу вычисления произведения чисел от 1 до N
+int N = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine(proiz(N));
+
+double proiz(double x)
+{
+    int p = 1;
+    for(int i = 1; i<=x; i++)
+    {
+        p = p*i;
+    
+    }
+    return p;
+}
