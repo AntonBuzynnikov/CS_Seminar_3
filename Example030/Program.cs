29cfdc34becd9f5fda6d636f39702e9bb6380418
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-04 08:40:05 +0700

    Complete example030

---
+++ Example030/Program.cs
@@ -0,0 +1,7 @@
+﻿// 30. Вывести на экран таблицу квадратов чисел от 1 до N
+int N = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Число\tКвадрат");
+for (int i = 1;i <= N; i++)
+{
+    System.Console.WriteLine("{0}\t{1}",i,Math.Pow(i,2));
+}
