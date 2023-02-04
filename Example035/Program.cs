72d78eeb9c8552da78c2428f1ca07d7af9276a62
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-04 09:06:34 +0700

    Complete example035

---
+++ Example035/Program.cs
@@ -0,0 +1,19 @@
+﻿// 35. Подсчитать сумму цифр в числе. Сделать подпрограмму
+string N = System.Console.ReadLine();
+int size = N.Length;
+int digit = Int32.Parse(N);
+
+double sumDigit(double x)
+{
+    double sum = 0;
+    for (int i = 1; i<=x; i++)
+    {
+        double d;
+        d = digit%10;
+        sum +=d;
+        digit/=10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(sumDigit(size));
