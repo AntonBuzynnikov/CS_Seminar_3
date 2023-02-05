5203190dd7a0323a608c437ae59ad900eae8eb66
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-05 09:03:58 +0700

    Complete example034

---
+++ Example034/Program.cs
@@ -0,0 +1,14 @@
+﻿// 34. Определить количество цифр в числе. Сделать подпрограмму
+int CounterDigits(int n)
+{
+    if (n==0) return 1;
+    int counter = 0;
+    while (n!=0)
+    {
+        counter++;
+        n/=10;
+    }
+    return counter;
+}
+int N = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine(CounterDigits(N));
